5bb0a866fb17da652c2530581e8fac31c97dd643
Kellton <kelltonl10@gmail.com>
2013-01-14 15:29:38 -0300

    Arquivos adicionados da Jeovania

---
+++ application/views/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<!-- The new doctype -->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Acens - Mentes em desenvolvimento</title>
+        <link rel="stylesheet" type="text/css" href="http://localhost/acens2013/css/style.css" />
+        <!-- Internet Explorer HTML5 enabling code: -->
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+
+        <style type="text/css">
+        .clear {
+        zoom: 1;
+        display: block;
+        }
+        .gradient {
+		filter: none;
+		}
+        </style>
+
+        <![endif]-->
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#sobre">Sobre</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfólio</a>
+                </li>
+                <li>
+                    <a href="#servicos">Serviços</a>
+                </li>
+                <li id="Home">
+                    <a href="#homeacens" class="homeIcon">Home</a>
+                </li>
+                <li>
+                    <a href="#contato">Contato</a>
+                </li>
+                <li>
+                    <a href="#extra">Extra</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+            </ul>
+        </nav>
+        
+        <header id="homeacens">
+            <div>
+                <hgroup>
+                    <h1>Acens</h1>
+                    <h2>Mentes em desenvolvimento</h2>
+                </hgroup>
+            </div>
+        </header>
+        
+        <section id="sobre">
+            <article>
+                <h1>Sobre a Acens</h1>
+                <div class="articleBody clear">
+                    <p>
+                        Elit vut turpis scelerisque pulvinar ultricies magna sit amet hac placerat placerat auctor elementum nec arcu dapibus dictumst elit enim sed? Amet, mauris enim, amet hac turpis, velit, ac vel? Aliquam sed, dis porttitor non arcu, mid ac, purus scelerisque? Adipiscing rhoncus urna porta nisi nisi montes sociis tortor adipiscing porttitor hac, egestas est lectus, sit turpis. Porttitor, dignissim, et, nunc augue eu ac porta, tincidunt? Augue habitasse in dapibus, aliquam scelerisque, pulvinar dignissim pulvinar magna phasellus magnis. Vel est, dis non porttitor lorem, purus arcu tortor massa lacus facilisis! In, nisi? Odio, parturient dis lorem? Ac. Tempor, tincidunt hac etiam aliquam ultricies adipiscing. Tincidunt, pulvinar augue a proin a, elit ultrices platea urna, proin et sit, habitasse integer augue.
+					</p>
+					
+					<p>	
+						Integer integer vut risus, integer pellentesque auctor, ridiculus massa natoque nisi placerat ut! Eros nec, pid elementum etiam augue! Nascetur lacus ut aliquam, rhoncus. Etiam aenean sociis massa lectus enim parturient rhoncus. Sit mus massa aenean lacus, duis montes pulvinar augue eu sagittis sit ac tincidunt dapibus ridiculus mauris ut! Adipiscing rhoncus vel in sed turpis diam ridiculus cursus! A cras pellentesque in? Lectus rhoncus, sociis mid placerat, nascetur ridiculus arcu, cum cum quis! Lorem? Lundium nisi! Dolor tristique! Magna placerat scelerisque mauris montes penatibus mattis quis cras. Enim? Ut mattis montes, nunc porttitor nunc, quis nascetur et turpis, velit placerat! Sed scelerisque, urna porttitor risus nisi hac ridiculus! Et sit facilisis proin lorem nunc amet, elementum sed cursus.
+                    </p>
+                    
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus quam quis nibh fringilla sit amet consectetur lectus malesuada. Sed nec libero erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mi nisi, rhoncus ut vestibulum ac, sollicitudin quis lorem. Duis felis dui, vulputate nec adipiscing nec, interdum vel tortor. Sed gravida, erat nec rutrum tincidunt, metus mauris imperdiet nunc, et elementum tortor nunc at eros. Donec malesuada congue molestie. Suspendisse potenti. Vestibulum cursus congue sem et feugiat. Morbi quis elit odio.
+                    </p>
+                </div>
+            </article>
+        </section>
+
+        <section id="portfolio">
+            <article>
+                <h1>Portfólio</h1>
+                <div id="telafora" class="centro">
+                    <div id="teladentro">
+                        
+                    </div>
+                    <div id="postit"><a href="#">Veja mais<br /> dos nossos<br />trabalhos!</a></div>
+                </div>
+                <div id="baseport" class="centro"><!----></div>
+                <div id="mesa"><!----></div>
+                <div id="mesa-borda" class="gradient"><!----></div>
+            </article>
+        </section>
+
+        <section id="servicos">
+            <article>
+                <h1>Serviços</h1>
+                
+                <div class="centro largurabase">
+                <a href="#" class="circular" target="_blank">
+					<div id="webcirculo" class="circular servicocirculo"><!-- --></div>
+				</a>
+				<a href="#" class="circular" target="_blank">
+					<div id="webcirculo" class="circular servicocirculo"><!-- --></div>
+				</a>
+				<a href="#" class="circular" target="_blank">
+					<div id="webcirculo" class="circular servicocirculo"><!-- --></div>
+				</a>
+				<div class="clear"><!----></div>
+				</div>
+            </article>
+        </section>
+
+        <section id="contato">
+            <article>
+                <hgroup>
+                    <h1>Contato</h1>
+                    <h2>Texto sobr contato etc....</h2>
+                </hgroup>
+                <form>
+                    <label>Nome:
+                        <input type="text" name="nome" />
+                    </label>
+                    <br />
+                    <label>E-mail:
+                        <input type="email" name="email" />
+                    </label>
+                    <br />
+                    <textarea name="menssagem" cols="40" rows="5">
+                </textarea><br />         
+                <input type="button" value="Enviar" />
+                </form>
+            </article>
+        </section>
+       
+       
+        <footer id="extra">
+            <div class="center">
+                <p>
+                    2013 - Desenvolvido por Acens - Todos os direitos reservados. 
+                </p>
+            </div>
+        </footer>
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js">/**/</script>
+        <script src="http://localhost/acens2013/js/jquery.scrollTo-min.js">/**/</script>
+        <script src="http://localhost/acens2013/js/script.js">/**/</script>
+    </body>
+</html>
