828f0db177eb4e7e2ed6ca3d600a0dd9a0d1081b
Jeovania <jeovaniaabreu@gmail.com>
2013-01-25 17:05:32 -0300

    Adicionado efeito lightbox em servicos

--- application/views/includes/servicos.php
+++ application/views/includes/servicos.php
@@ -1,12 +1,36 @@
 <section id="servicos">
 	<article>
 		<h1>Serviços</h1>
-
+		
+		<div class="backdrop"></div>
+		<div id="webservice_conteudo" class="box centro">
+			<div class="close">
+				[X] FECHAR
+			</div>
+			<h1>Sistemas Web</h1>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta nisi cum culpa beatae excepturi sed nihil dolor labore unde pariatur omnis non aut deserunt quas voluptas corporis ipsa saepe itaque.
+		</div>
+		
+		<div id="deskservice_conteudo" class="box centro">
+			<div class="close">
+				[X] FECHAR
+			</div>
+			<h1>Sistemas Desktop</h1>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta nisi cum culpa beatae excepturi sed nihil dolor labore unde pariatur omnis non aut deserunt quas voluptas corporis ipsa saepe itaque.
+		</div>
+		
+		<div id="midiasservice_conteudo" class="box centro">
+			<div class="close">
+				[X] FECHAR
+			</div>
+			<h1>Gerencia em Mídias Sociais</h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta nisi cum culpa beatae excepturi sed nihil dolor labore unde pariatur omnis non aut deserunt quas voluptas corporis ipsa saepe itaque.
+		</div>	
+		
 		<div class="centro largurabase">
 			<ul>
 				<li class="servico">
 					<a href="#">
-					<div id="webcirculo" class="circular servicocirculo"><img src="<?php echo base_url(); ?>/img/sistemasweb.png"/>
+					<div id="webcirculo" class="circular servicocirculo lightbox"><img src="<?php echo base_url(); ?>/img/sistemasweb.png"/>
 					</div></a>
 					<span>Sites e Sistemas web</span>
 				</li>
@@ -23,8 +47,6 @@
 					<span>Gerencia de Mídias Sociais</span>
 				</li>
 			</ul>
-
-
 		</div>
 	</article>
 </section>
