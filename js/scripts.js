458227444e91e3cb3fa467d77a35609bf0b48c7f
Jeovania <jeovaniaabreu@gmail.com>
2013-01-27 17:38:14 -0300

    pagina servicos modificada e javascript minificado

---
+++ js/scripts.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+	/*SCROLL DA PÁGINA*/
+	$('nav a').click(function(e) {
+		$.scrollTo(this.hash || 0, 500);
+		e.preventDefault();
+	});
+
+	/*MENU*/
+	var windowH = $(window).height();
+	var stickToBot = windowH - $('#menuWrapper').outerHeight(true);
+
+	$('#menuWrapper').css({
+		'top' : stickToBot + 'px'
+	});
+
+	$(window).scroll(function() {
+		var scrollVal = $(this).scrollTop();
+		if (scrollVal > stickToBot) {
+			$('#menuWrapper').css({
+				'position' : 'fixed',
+				'top' : '0px'
+			});
+			$('#home').animate({
+				'top' : 10,
+			}, 0);
+
+			$('#home').attr('class', 'logosombra');
+		} else {
+			$('#menuWrapper').css({
+				'position' : 'absolute',
+				'top' : stickToBot + 'px'
+			});
+			$('#home').animate({
+				'top' : -33,
+			}, 0);
+
+			$('#home').removeAttr('class', 'logosombra');
+		}
+	});
+
+	/*Fancybox*/
+	$('.fancybox').fancybox({
+		openEffect : 'fade',
+		closeEffect : 'fade',
+		helpers : {
+			buttons : {
+				position : 'top'
+			},
+			overlay : {
+				css : {
+					'background' : 'rgba(0, 0, 0, 0.85)'
+				}
+			}
+		}
+	});
+
+	/*ANIMAÇÃO DA LOGO - PÁGINA INICIAL*/
+	$('.seta3').delay(200).fadeIn(800);
+	$('.seta2').delay(1000).fadeIn(800);
+	$('.seta1').delay(1800).fadeIn(800);
+	$('.circulologo').delay(200).easyPieChart({
+		barColor : '#FFFFFF',
+		trackColor : '#0A2E4E',
+		scaleColor : false,
+		lineCap : 'butt',
+		lineWidth : 10,
+		animate : 2000,
+		size : 250
+	});
+	$('.home_text1').delay(200).animate({
+		'margin-left' : 20,
+		'opacity' : 1
+	}, 800);
+	$('.home_text2').delay(1000).animate({
+		'margin-left' : 30,
+		'opacity' : 1
+	}, 800);
+	$('.home_text3').delay(1800).animate({
+		'margin-left' : 20,
+		'opacity' : 1
+	}, 800);
+
+	/*Mapa google*/
+	$('#map').gmap().bind('init', function(ev, map) {
+		$('#map').gmap('addMarker', {
+			'position' : '57.7973333,12.0502107',
+			'bounds' : true
+		}).click(function() {
+			$('#map').gmap('openInfoWindow', {
+				'content' : 'Acens'
+			}, this);
+		});
+	});
+
+	/*EFEITOS HOVER DE SERVIÇOS*/
+	$('#deskservice').hover(function() {
+		this.src = this.src.replace("sistemasdesktop", "sistemasdesktophover");
+	}, function() {
+		this.src = this.src.replace("sistemasdesktophover", "sistemasdesktop");
+	});
+
+	$('#webservice').hover(function() {
+		this.src = this.src.replace("sistemasweb", "sistemaswebhover");
+	}, function() {
+		this.src = this.src.replace("sistemaswebhover", "sistemasweb");
+	});
+
+	$('#midiaservice').hover(function() {
+		this.src = this.src.replace("midiassociais", "midiassociaishover");
+	}, function() {
+		this.src = this.src.replace("midiassociaishover", "midiassociais");
+	});
+});
