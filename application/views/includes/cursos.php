421acfd35858c1459b28f11d28f4aa636038cfc4
Jeovania <jeovaniaabreu@gmail.com>
2013-01-27 18:53:24 -0300

    Criado layout da pag cursos e a funcao de ocultar a pag

--- application/views/includes/cursos.php
+++ application/views/includes/cursos.php
@@ -1,3 +1,53 @@
-        <section id="cursos">
-            <h1>Nossos cursos</h1>
-        </section>
+<section id="cursos">
+	<h1>Nossos cursos</h1>
+	<?php
+	$jornada = true;
+	if ($jornada == true):
+	?>
+	<a href="<?php echo base_url(); ?>teste.html" class="botaojornada centro fancybox fancybox.iframe">Inscreva-se na Jornada de cursos</a>
+	<?php
+	endif;
+	?>
+	<ul class="largurabase centro">
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+		<li class="circular">
+			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
+				<img src="<?php echo base_url(); ?>/img/cursonaodisponivel.png" alt="" />
+				</a>
+		</li>
+	</ul>
+</section>
--- application/views/index.php
+++ application/views/index.php
@@ -1,10 +1,14 @@
 <?php 
+$temcurso = true; /*Mostrar ou ocultar a página de cursos*/
+
 $this->load->view('includes/header');
 $this->load->view('includes/menu');
 $this->load->view('includes/home');
 $this->load->view('includes/sobre');
 $this->load->view('includes/portfolio');
 $this->load->view('includes/servicos');
-$this->load->view('includes/cursos');
+if ($temcurso){
+	$this->load->view('includes/cursos');
+}
 $this->load->view('includes/contato');
 ?>
