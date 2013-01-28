ed62c7a00a3b7c4520b477c7a0ddeb477f77aba8
EduardoMB <eduardomb.uece@gmail.com>
2013-01-28 17:17:01 -0300

    Terminada a pagina sobre

--- application/views/includes/sobre.php
+++ application/views/includes/sobre.php
@@ -4,11 +4,25 @@
             	<div id="imagem"></div>
                 <div class="texto_sobre">
                     <b>A Acens</b>
-                    <p>&nbsp; &nbsp; &nbsp; A Acens - Empresa Júnior - foi fundada em 28 de março de 2008, sendo uma iniciativa empreendedora e ousada de um grupo de aluno do curso de Ciências da Computação da Universidade Estadual do Ceará(UECE).
-                        Nossa missão é: "Toranar-se referência do MEJ nacional, oferecer serviços de quaidade e buscar reconhecimento pelo departamento de computação da UECE como uma fonto de capacitação para os alunos da graduação".</p>
+                    <p>&nbsp; &nbsp; &nbsp; A Acens - Empresa Júnior - foi fundada em 28 de março de 2008, sendo uma iniciativa empreendedora e ousada de um grupo de aluno do curso de Ciências da Computação da Universidade Estadual do Ceará(UECE). Nossa missão é: "Tornar-se referência do MEJ nacional, oferecer serviços de quaidade e buscar reconhecimento pelo departamento de computação da UECE como uma fonte de capacitação para os alunos da graduação".</p>
+                    <br />
                     <b>O que fazemos</b>
-                    <p> &nbsp; &nbsp; &nbsp;Atuando há 5 anos na área de TI, a Acens vem facilitando o dia a dia de empresas, oferecendo produtos como o desenvolvimento de sites, sistemas web, cursos in company, treinamento e capacitação em TI.    
-                        Sempre atentos ao mercado de trabalho ultizamos o que há de mais moderno em ferramentas de desenvolvimento, sempre visando a qualidade de nosso serviços e a satisfação dos nossos clientes.</p>
+                    <p> &nbsp; &nbsp; &nbsp;Atuando há 5 anos na área de TI, a Acens vem facilitando o dia a dia de empresas, oferecendo produtos como o desenvolvimento de sites, sistemas web, treinamento e capacitação em TI. Sempre atentos ao mercado de trabalho ultizamos o que há de mais moderno em ferramentas de desenvolvimento, sempre visando a qualidade de nosso serviços e a satisfação dos nossos clientes.</p>
+                </div>
+                <h2>O que mais voçê precisa saber:</h2>
+                <div id="saiba_mais" class="centro">
+	                <div id="secao_ej">
+	                	<p id="texto_ej">Conheça a FEJECE</p>
+	                	<a id="botao_ej" class="botaojornada">Saiba mais...</a>
+	                </div>
+	                <div id="secao_bj">
+	                	<p id="texto_bj">Conheça a Brasil Junior</p>
+	                	<a id="botao_bj" class="botaojornada">Saiba mais...</a>
+	                </div>
+	                <div id="secao_mej">
+	                	<p id="texto_mej">Fazemos parte do MEJ</p>
+	                	<a id="botao_mej" class="botaojornada">Saiba mais...</a>
+	                </div>
                 </div>
             </article>
         </section>
