7cb5e7eda00e1cf65caa6ba1bf703e780e153feb
Eduardo <eduardomb.uece@gmail.com>
2013-02-01 01:27:31 -0300

    Validacao para campos de formulario concluida

--- application/views/includes/contato.php
+++ application/views/includes/contato.php
@@ -10,12 +10,24 @@
                             Entre em contato conosco
                         </legend>
                         <?php
-                            echo form_open('includes/contato');
-                            echo form_input(array('class'=>'form','name'=>'nome','placeholder'=>'NOME',),'','');
-                            echo form_input(array('class'=>'form','type'=>'email','name'=>'email','placeholder'=>'E-MAIL',),'','');
-                            echo form_textarea(array('id'=>'mensagem','name'=>'mensagem','cols'=>'48','rows'=>'6','placeholder'=>'MENSAGEM',),'','');
+                        
+                        	function checar_email($string) {
+								if (stristr($string, '@gmail.com') != FALSE) return TRUE;
+								if (stristr($string, '@hotmail.com') != FALSE) return TRUE;
+								
+								$this->form_validation->set_message(email,'Digite um e-mail válido!');
+								return false;
+							} 
+                        
+                            echo form_open();
+                            echo form_input(array('class'=>'form','name'=>'nome','placeholder'=>'Nome','required'=>'','pattern'=>'.{3,10}', 'title'=> 'Mínimo de 3 caracteres e máximo de 10'));
+                            echo form_input(array('class'=>'form','type'=>'email','name'=>'email','placeholder'=>'E-mail','required'=>'','pattern'=>'[a-z|A-Z]{3,}(.[a-z|A-Z]+)?@gmail.com|[a-z|A-Z]{3,}(.[a-z|A-Z]+)?@hotmail.com|', 'title'=> 'Digite um e-mail válido.'));
+                            echo form_textarea(array('id'=>'mensagem','name'=>'mensagem','cols'=>'48','rows'=>'6','placeholder'=>'Digite sua mensagem',));
                             echo form_submit(array('id'=>'formsubmit',),'Enviar');
                             echo form_close();
+							$this->form_validation->set_rules('nome', 'Nome', 'required');
+							//$this->form_validation->set_rules('email', 'e-mail', '|required|min_length[5]|');
+							$this->form_validation->set_rules('email', 'e-mail', 'callback_checar_email');
                         ?>
                     </fieldset>
                 </div>
