f97bad2c50402b8f1222daead2ec4b314a74cda5
Jeovania <jeovaniaabreu@gmail.com>
2013-02-03 22:52:11 -0300

    Cor do footer e botoes modificados

--- application/views/includes/contato.php
+++ application/views/includes/contato.php
@@ -1,43 +1,46 @@
-        <footer id="contato">
-            <article>
-                <hgroup>
-                    <h1>Contato</h1>
-                    <h2>Texto sobr contato etc....</h2>
-                </hgroup>
-                <div id="contato_local">
-                    <fieldset id="fieldsetContato">
-                        <legend>
-                            Entre em contato conosco
-                        </legend>
-                        <?php
-                        
-                        	function checar_email($string) {
-								if (stristr($string, '@gmail.com') != FALSE) return TRUE;
-								if (stristr($string, '@hotmail.com') != FALSE) return TRUE;
-								
-								$this->form_validation->set_message(email,'Digite um e-mail válido!');
-								return false;
-							} 
-                        
-                            echo form_open();
-                            echo form_input(array('class'=>'form','name'=>'nome','placeholder'=>'Nome','required'=>'','pattern'=>'.{3,10}', 'title'=> 'Mínimo de 3 caracteres e máximo de 10'));
-                            echo form_input(array('class'=>'form','type'=>'email','name'=>'email','placeholder'=>'E-mail','required'=>'','pattern'=>'[a-z|A-Z]{3,}(.[a-z|A-Z]+)?@gmail.com|[a-z|A-Z]{3,}(.[a-z|A-Z]+)?@hotmail.com|', 'title'=> 'Digite um e-mail válido.'));
-                            echo form_textarea(array('id'=>'mensagem','name'=>'mensagem','cols'=>'48','rows'=>'6','placeholder'=>'Digite sua mensagem',));
-                            echo form_submit(array('id'=>'formsubmit',),'Enviar');
-                            echo form_close();
-                        ?>
-                    </fieldset>
-                </div>
-                <div id="map"></div>
-            </article>  
-            <?php
-                include "newsletter.php";
-            ?>
-            <div class="center">
-                <p>
-                    2013 - Desenvolvido por Acens - Todos os direitos reservados. 
-                </p>
-            </div>
-        </footer>
-    </body>
+<footer id="contato">
+	<article id="contato">
+		<hgroup>
+			<h1>Contato</h1>
+		</hgroup>
+		<div id="contato_local">
+			<div id="fieldsetContato">
+				<legend>
+					Entre em contato conosco
+				</legend>
+				<?php
+				function checar_email($string) {
+					if (stristr($string, '@gmail.com') != FALSE)
+						return TRUE;
+					if (stristr($string, '@hotmail.com') != FALSE)
+						return TRUE;
+
+					$this -> form_validation -> set_message(email, 'Digite um e-mail válido!');
+					return false;
+				}
+
+				echo form_open();
+				echo form_input(array('class' => 'form', 'name' => 'nome', 'placeholder' => 'Nome', 'required' => '', 'pattern' => '.{3,10}', 'title' => 'Mínimo de 3 caracteres e máximo de 10'));
+				echo form_input(array('class' => 'form', 'type' => 'email', 'name' => 'email', 'placeholder' => 'E-mail', 'required' => '', 'pattern' => '[a-z|A-Z]{3,}(.[a-z|A-Z]+)?@gmail.com|[a-z|A-Z]{3,}(.[a-z|A-Z]+)?@hotmail.com|', 'title' => 'Digite um e-mail válido.'));
+				echo form_textarea(array('id' => 'mensagem', 'name' => 'mensagem', 'cols' => '48', 'rows' => '6', 'placeholder' => 'Digite sua mensagem', ));
+				echo form_submit(array('id' => 'formsubmit', ), 'Enviar');
+				echo form_close();
+				?>
+			</div>
+		</div>
+	</article>
+	<article id="localizacao">
+	<h1>Localização</h1>	
+		<div id="map"></div>
+	</article>
+	<?php
+	include "newsletter.php";
+	?>
+</footer>
+<div id="creditos" class="center">
+	<p>
+		2013 - Desenvolvido por Acens - Todos os direitos reservados.
+	</p>
+</div>
+</body>
 </html>
--- application/views/includes/cursos.php
+++ application/views/includes/cursos.php
@@ -1,13 +1,5 @@
 <section id="cursos">
 	<h1>Nossos cursos</h1>
-	<?php
-	$jornada = true;
-	if ($jornada == true):
-	?>
-	<a href="<?php echo base_url(); ?>teste.html" class="botaojornada centro fancybox fancybox.iframe">Inscreva-se na Jornada de cursos</a>
-	<?php
-	endif;
-	?>
 	<ul class="largurabase centro">
 		<li class="circular">
 			<a href="<?php echo base_url(); ?>teste.html" class="fancybox fancybox.iframe">
@@ -50,4 +42,12 @@
 				</a>
 		</li>
 	</ul>
+	<?php
+	$jornada = true;
+	if ($jornada == true):
+	?>
+	<a href="<?php echo base_url(); ?>teste.html" class="botaojornada centro fancybox fancybox.iframe">Inscreva-se na Jornada de cursos</a>
+	<?php
+	endif;
+	?>
 </section>
