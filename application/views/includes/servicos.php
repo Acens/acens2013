ee5b4b61985494a42665f80524f9547959cffb45
Jeovania <jeovaniaabreu@gmail.com>
2013-03-05 11:25:25 -0300

    Nova popup adicionada - Colorbox plugin

--- application/views/includes/servicos.php
+++ application/views/includes/servicos.php
@@ -5,14 +5,14 @@
 	</hgroup>
 	<ul class="ca-menu largurabase centro">
 		<li>
-			<a class='inline' href="#inline_content"> <span class="ca-icon"><img src="<?php echo base_url(); ?>/img/sistemasdesktop.png" alt="" /></span>
+			<a class='inline' href="#conteudo_sistemasd"> <span class="ca-icon"><img src="<?php echo base_url(); ?>/img/sistemasdesktop.png" alt="" /></span>
 			<div class="ca-content">
 				<h2 class="ca-main">Sistemas Desktop</h2>
 				<h3 class="ca-sub">Nossos sistemas facilitando seus negócios.</h3>
 			</div> </a>
 		</li>
 		<li>
-			<a class='inline' href="#inline_content"> <span class="ca-icon transicao"><img src="<?php echo base_url(); ?>/img/sistemasweb.png" alt="" /></span>
+			<a class='inline' href="#conteudo_web"> <span class="ca-icon transicao"><img src="<?php echo base_url(); ?>/img/sistemasweb.png" alt="" /></span>
 			<div class="ca-content">
 				<h2 class="ca-main transicao">Sites, Sistemas Web e e-Commerce</h2>
 				<h3 class="ca-sub transicao">Seu site feito com as melhores ferramentas.</h3>
@@ -20,7 +20,7 @@
 		</li>
 		<li>
 			<img src="<?php echo base_url(); ?>/img/novo.png" alt="Novidade" class="embreve_novo"/>
-			<a class='inline' href="#inline_content"> <span class="ca-icon"><img src="<?php echo base_url(); ?>/img/dispmoveis.png" alt=""/></span>
+			<a class='inline' href="#conteudo_mobile"> <span class="ca-icon"><img src="<?php echo base_url(); ?>/img/dispmoveis.png" alt=""/></span>
 			<div class="ca-content">
 				<h2 class="ca-main">Aplicativos para Dispositivos Móveis</h2>
 				<h3 class="ca-sub">Em breve!</h3>
@@ -28,7 +28,7 @@
 		</li>
 		<li>
 			<img src="<?php echo base_url(); ?>/img/novo.png" alt="Novidade" class="embreve_novo"/>
-			<a class='inline' href="#inline_content"> <span class="ca-icon"><img src="<?php echo base_url(); ?>/img/midiassociais.png" alt=""/></span>
+			<a class='inline' href="#conteudo_sociais"> <span class="ca-icon"><img src="<?php echo base_url(); ?>/img/midiassociais.png" alt=""/></span>
 			<div class="ca-content">
 				<h2 class="ca-main">Gerência de Mídias Digitais</h2>
 				<h3 class="ca-sub">Suas mídias sociais e sua marca no topo.</h3>
@@ -38,14 +38,29 @@
 	
 		<!-- This contains the hidden content for inline calls -->
 		<div style='display:none'>
-			<div id='inline_content' style='padding:10px; background:#fff;'>
-			<p><strong>This content comes from a hidden element on this page.</strong></p>
-			<p>The inline option preserves bound JavaScript events and changes, and it puts the content back where it came from when it is closed.</p>
-			<p><a id="click" href="#" style='padding:5px; background:#ccc;'>Click me, it will be preserved!</a></p>
-			
-			<p><strong>If you try to open a new ColorBox while it is already open, it will update itself with the new content.</strong></p>
-			<p>Updating Content Example:<br />
-			<a class="ajax" href="../content/ajax.html">Click here to load new content</a></p>
+			<div id='conteudo_sistemasd' class="popup_conteudo">
+
 			</div>
 		</div>
+		
+		<!-- This contains the hidden content for inline calls -->
+		<div style='display:none'>
+			<div id='conteudo_web' class="popup_conteudo">
+
+			</div>
+		</div>
+		
+		<!-- This contains the hidden content for inline calls -->
+		<div style='display:none'>
+			<div id='conteudo_mobile' class="popup_conteudo">
+
+			</div>
+		</div>
+		
+		<!-- This contains the hidden content for inline calls -->
+		<div style='display:none'>
+			<div id='conteudo_sociais' class="popup_conteudo">
+
+			</div>
+		</div>						
 </section>
--- js/scripts.js
+++ js/scripts.js
@@ -101,8 +101,8 @@ $(document).ready(function() {
 
 	/*ANIMAÇÃO DA LOGO - PÁGINA INICIAL*/
 	$('.seta3').delay(200).fadeIn(800);
-	$('.seta2').delay(1000).fadeIn(800);
-	$('.seta1').delay(1800).fadeIn(800);
+	$('.seta2').delay(1200).fadeIn(800);
+	$('.seta1').delay(2000).fadeIn(800);
 	$('.circulologo').delay(200).easyPieChart({
 		barColor : '#FFFFFF',
 		trackColor : '#0A2E4E',
@@ -112,15 +112,15 @@ $(document).ready(function() {
 		animate : 2000,
 		size : 250
 	});
-	$('.home_text1').delay(200).animate({
+	$('.home_text1').delay(1200).animate({
 		'margin-left' : 20,
 		'opacity' : 1
 	}, 800);
-	$('.home_text2').delay(1000).animate({
+	$('.home_text2').delay(2000).animate({
 		'margin-left' : 30,
 		'opacity' : 1
 	}, 800);
-	$('.home_text3').delay(1800).animate({
+	$('.home_text3').delay(2800).animate({
 		'margin-left' : 20,
 		'opacity' : 1
 	}, 800);
